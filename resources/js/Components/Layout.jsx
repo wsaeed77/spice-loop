7758b999daa92c458b63d17794e9cc4ad9e1596e
Waqas Saeed <w.saeed77@gmail.com>
2026-02-05 16:51:26 +0000

    added script

--- resources/js/Components/Layout.jsx
+++ resources/js/Components/Layout.jsx
@@ -1,6 +1,6 @@
 import { Link, usePage, router } from '@inertiajs/react';
 import { useState, useEffect } from 'react';
-import { useState, } from 'react';
+
 import { useCart } from '../contexts/CartContext';
 
 
